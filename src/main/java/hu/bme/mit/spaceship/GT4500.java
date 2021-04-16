81e368e63dd8dac8175cfeeb5eddf691c162ff72
Hegedüs Dániel <hegeda114@gmail.com>
2021-04-16 21:31:06 +0200

    case ALL fixed

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,10 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+        boolean primarySuccess = primaryTorpedoStore.fire(1);
+        boolean secondarySuccess = secondaryTorpedoStore.fire(1);
+        if(primarySuccess && secondarySuccess)
+          firingSuccess = true;
 
         break;
     }
