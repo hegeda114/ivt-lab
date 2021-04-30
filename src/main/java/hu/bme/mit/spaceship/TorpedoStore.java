a978a745efbbec095121cf3135e5193dd79f37d6
unknown <hegeda114@gmail.com>
2021-04-30 12:33:57 +0200

    Random fixed

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,6 +14,7 @@ public class TorpedoStore {
   int y = 7;
   int x = 14;
   private int torpedoCount = 0;
+  Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
@@ -37,7 +38,6 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
